6871ef964cfd5f61b61716a85bb1581be5d4aa01
amanDhiran <amandhiran201@gmail.com>
2024-04-28 23:51:52 +0530

    fix the publish page so that it disables while blog is being created

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,60 +1,104 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { LuPenSquare } from "react-icons/lu";
-import { Link, useNavigate } from 'react-router-dom';
-import BACKEND_URL from '../config';
+import { Link, useNavigate } from "react-router-dom";
+import BACKEND_URL from "../config";
 
 export interface User {
-  id: string,
-  name: string,
+  id: string;
+  name: string;
 }
 
-function Navbar({handleClick, fromCreatePage}: {handleClick?: () => void, fromCreatePage?: boolean}) {
-  const [show, setShow] =useState(false)
-  const [data, setData] = useState<User>()
-  // const [loading, setLoading] = useState(true) 
-  const [disabled, setDisabled] = useState(false)
+function Navbar({
+  loading,
+  handleClick,
+  fromCreatePage,
+}: {
+  loading?: boolean;
+  handleClick?: () => void;
+  fromCreatePage?: boolean;
+}) {
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState<User>();
 
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get(`${BACKEND_URL}/user/me`, {
-      headers: {
-          Authorization: 'bearer ' + localStorage.getItem("token")
-      }
-  }).then(res => {
-      setData(res.data.user)
-      // setLoading(false)
-      // console.log(data);
-      
-  }).catch(err => console.log(err, "there was an error while fetching blogs")
-  )
-  }, [])
+    axios
+      .get(`${BACKEND_URL}/user/me`, {
+        headers: {
+          Authorization: "bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        setData(res.data.user);
+        // setLoading(false)
+        // console.log(data);
+      })
+      .catch((err) =>
+        console.log(err, "there was an error while fetching blogs")
+      );
+  }, []);
 
-  function handleLogOut (){
+  function handleLogOut() {
     localStorage.removeItem("token");
-    navigate('/signin')
+    navigate("/signin");
   }
 
   return (
-    <div className='border-b border-gray-700/20 px-4 py-3 relative md:py-5'>
-        <div className='text-secondary flex justify-between  items-center lg:mx-16 mx-8 '> 
-            <Link to={'/'} className=' text-3xl font-bold'>
-                Articles
+    <div className="border-b border-gray-700/20 px-4 py-3 relative md:py-5">
+      <div className="text-secondary flex justify-between  items-center lg:mx-16 mx-8 ">
+        <Link to={"/"} className=" text-3xl font-bold">
+          Articles
+        </Link>
+        <div className="flex items-center  gap-4">
+          {fromCreatePage ? (
+            <button
+              className="bg-green-600 rounded-md hover:bg-green-500 disabled:text-gray-400 font-semibold disabled:bg-green-600/60 p-2"
+              onClick={handleClick}
+              disabled={loading}
+            >
+              Publish
+            </button>
+          ) : (
+            <Link
+              to={"/new-blog"}
+              className="flex font-light text-slate-200/70 hover:text-secondary items-center gap-2"
+            >
+              <LuPenSquare className="text-xl" />
+              Write
             </Link>
-            <div className='flex items-center  gap-4'>
-                  {fromCreatePage ? <button className='bg-green-600 rounded-md p-2' onClick={handleClick}>Publish</button> : <Link to={'/new-blog'} className='flex font-light text-slate-200/70 hover:text-secondary items-center gap-2'><LuPenSquare className='text-xl' />Write</Link>
-                  }
-                <div onClick={() => setShow((prev) => !prev) } className="h-8 w-8 items-center bg-border rounded-[50%] flex flex-col justify-center hover:cursor-pointer">{data?.name[0].toUpperCase()}</div>
-            </div>
+          )}
+          <div
+            onClick={() => setShow((prev) => !prev)}
+            className="h-8 w-8 items-center bg-border rounded-[50%] flex flex-col justify-center hover:cursor-pointer"
+          >
+            {data?.name[0].toUpperCase()}
+          </div>
         </div>
-        {show && <div className={` absolute -bottom-20 bg-primary text-secondary right-[1px] lg:right-16 border px-5  py-3 rounded  border-slate-800/60 w-52 lg:w-60 flex flex-col z-10 gap-2 transition-all ease-in-out duration-300 ${show ? " opacity-100 " : "opacity-0 "}`}>
-          <Link to={'/profile'} className='transition-all ease-in-out duration-300 text-slate-200/70 hover:text-secondary'>Profile</Link>
-          <div onClick={handleLogOut} className='transition-all cursor-pointer ease-in-out duration-300 text-red-600/85 hover:text-red-600'>Log Out</div>
-        </div>}
+      </div>
+      {show && (
+        <div
+          className={` absolute -bottom-20 bg-primary text-secondary right-[1px] lg:right-16 border px-5  py-3 rounded  border-slate-800/60 w-52 lg:w-60 flex flex-col z-10 gap-2 transition-all ease-in-out duration-300 ${
+            show ? " opacity-100 " : "opacity-0 "
+          }`}
+        >
+          <Link
+            to={"/profile"}
+            className="transition-all ease-in-out duration-300 text-slate-200/70 hover:text-secondary"
+          >
+            Profile
+          </Link>
+          <div
+            onClick={handleLogOut}
+            className="transition-all cursor-pointer ease-in-out duration-300 text-red-600/85 hover:text-red-600"
+          >
+            Log Out
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/CreateBlog.tsx
+++ frontend/src/pages/CreateBlog.tsx
@@ -1,77 +1,90 @@
-import React, { useState } from 'react'
-import { useEditor } from '@tiptap/react'
-import Navbar from '../components/Navbar'
-import TextEditor from '../components/TextEditor'
-import CodeBlock from '@tiptap/extension-code-block'
+import React, { useState } from "react";
+import { useEditor } from "@tiptap/react";
+import Navbar from "../components/Navbar";
+import TextEditor from "../components/TextEditor";
+import CodeBlock from "@tiptap/extension-code-block";
 
-import StarterKit from '@tiptap/starter-kit'
-import Placeholder from '@tiptap/extension-placeholder'
-import { CreateBlogInput } from '@aman.dev/common'
-import axios from 'axios'
-import BACKEND_URL from '../config'
-import { useNavigate } from 'react-router-dom'
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import { CreateBlogInput } from "@aman.dev/common";
+import axios from "axios";
+import BACKEND_URL from "../config";
+import { useNavigate } from "react-router-dom";
 
 function CreateBlog() {
-    const [blogData, setBlogData] = useState<CreateBlogInput>({
-        title: "",
-        content: ""
-      })
-    
-      const navigate= useNavigate()
+  const [blogData, setBlogData] = useState<CreateBlogInput>({
+    title: "",
+    content: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-    const editor = useEditor({
-        extensions: [
-          StarterKit,
-          Placeholder.configure({
-            placeholder: 'Tell your story...',
-          }),
-          CodeBlock
-        ],
-        
-        editorProps: {
-          attributes: {
-            class: ' focus:outline-none h-20',
-          },
+  const navigate = useNavigate();
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Tell your story...",
+      }),
+      CodeBlock,
+    ],
+
+    editorProps: {
+      attributes: {
+        class: " focus:outline-none h-20",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      setBlogData({
+        ...blogData,
+        content: html,
+      });
+    },
+  });
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true)
+      await axios.post(`${BACKEND_URL}/blog`, blogData, {
+        headers: {
+          Authorization: "bearer " + localStorage.getItem("token"),
         },
-        onUpdate: ({ editor }) => {
-            const html = editor.getHTML()
-            setBlogData({
-                ...blogData,
-                content: html
-            })
-        }
-        
-      })
-
-
-      const handleSubmit =async() => {
-        await axios.post(`${BACKEND_URL}/blog`,blogData,{
-            headers: {
-                Authorization: "bearer " + localStorage.getItem("token")
-        }} )
-        navigate('/')
-      }
+      });  
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false)
+    }
+    
+    navigate("/");
+  };
 
   return (
     <>
-        <Navbar handleClick={handleSubmit} fromCreatePage={true} />
-        <div className="text-secondary lg:mx-24 mx-8 mt-5 ">
-          <div className="mt-5 flex flex-col gap-3 lg:max-w-[1100px]">
+      <Navbar loading={loading} handleClick={handleSubmit} fromCreatePage={true} />
+      <div className="text-secondary lg:mx-24 mx-8 mt-5 ">
+        <div className="mt-5 flex flex-col gap-3 lg:max-w-[1100px]">
           <div className="md:text-5xl text-3xl font-bold">
-            <input type="text" className='w-full outline-none bg-primary' placeholder='Title' onChange={(e) => setBlogData({
-                ...blogData,
-                title: e.target.value
-            })}/>
+            <input
+              type="text"
+              className="w-full outline-none bg-primary"
+              placeholder="Title"
+              onChange={(e) =>
+                setBlogData({
+                  ...blogData,
+                  title: e.target.value,
+                })
+              }
+            />
           </div>
           <div className="leading-7 editorJs text-secondary">
             <TextEditor editor={editor} />
           </div>
         </div>
-        
       </div>
     </>
-    
-  )
+  );
 }
 
-export default CreateBlog
+export default CreateBlog;
